f4289d2f42a77744152074ba9bb72aaf28e85330
3440CodeCampus <mtlgdroid@gmail.com>
2018-07-27 21:31:18 -0400

    advance feature added

--- catalog/admin.py
+++ catalog/admin.py
@@ -8,6 +8,9 @@ admin.site.register(BookInstance)
 admin.site.register(Genre)
 admin.site.register(Language)
 
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth',  'date_of_death')]
@@ -17,6 +20,8 @@ admin.site.register(Author, AuthorAdmin)
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
@@ -31,4 +36,5 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields':('status', 'due_back')
         }),
     )
+
 
